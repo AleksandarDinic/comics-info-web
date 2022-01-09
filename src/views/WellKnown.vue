13654b9edd8d3e530ccb1ac1213f2194984fca3d
Aleksandar Dinic <a.dinic@icloud.com>
2022-01-09 21:59:07 +0100

    Add apple-app-site-association

---
+++ src/views/WellKnown.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'WellKnown',
+  props: {
+    file: String
+  },
+  mounted () {
+    window.location.href = this.file
+  }
+}
+</script>
